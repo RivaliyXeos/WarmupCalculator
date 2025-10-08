890d8ec30f44bd0c4147c28d9346be4927d8dbd2
Arthur M <massias.arthur@gmail.com>
2025-10-08 13:57:51 +0200

    Initial Commit

---
+++ WarmupCalculator/WarmupCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmupCalculatorApp.swift
+//  WarmupCalculator
+//
+//  Created by Arthur Massias on 07/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct WarmupCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WarmupCalculatorTests/WarmupCalculatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  WarmupCalculatorTests.swift
+//  WarmupCalculatorTests
+//
+//  Created by Arthur Massias on 07/09/2025.
+//
+
+import Testing
+
+struct WarmupCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
